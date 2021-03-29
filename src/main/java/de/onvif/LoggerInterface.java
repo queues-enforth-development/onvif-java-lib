5ad396e5473f692df556d329e5c8b8e52263b8f5
jmccay-work <jmccay@qed.com>
2021-03-29 08:17:41 -0400

    Added an interface class.

---
+++ src/main/java/de/onvif/LoggerInterface.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.onvif;
+
+/**
+ *
+ * @author jmccay
+ */
+public interface LoggerInterface {
+    LibLogger logger = new LibLogger();
+}
